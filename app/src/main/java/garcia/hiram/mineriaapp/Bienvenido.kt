9f0bfae1190275bb5473b01771b119b3729aa891
Hiram610 <hiram.garcia216629@potros.itson.edu.mx>
2023-05-04 12:28:09 -0700

    subiendo app movil

---
+++ app/src/main/java/garcia/hiram/mineriaapp/Bienvenido.kt
@@ -0,0 +1,11 @@
+package garcia.hiram.mineriaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Bienvenido : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bienvenido)
+    }
+}
---
+++ app/src/main/java/garcia/hiram/mineriaapp/Congestion.kt
@@ -0,0 +1,11 @@
+package garcia.hiram.mineriaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Congestion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_congestion)
+    }
+}
