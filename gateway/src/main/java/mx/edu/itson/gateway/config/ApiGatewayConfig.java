e83fe3409bcbe698d1ad56edbd59eb0290346502
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-05-14 18:36:52 -0700

    Se configuro endpoint

--- gateway/src/main/java/mx/edu/itson/gateway/config/ApiGatewayConfig.java
+++ gateway/src/main/java/mx/edu/itson/gateway/config/ApiGatewayConfig.java
@@ -1,18 +1,41 @@
 package mx.edu.itson.gateway.config;
 
+import java.util.List;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @Configuration
 public class ApiGatewayConfig {
-
+   
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/product/**").uri("http://localhost:5000"))
-                .route(r -> r.path("/cliente/**").uri("http://localhost:5003"))
+                .route(r -> r.path("/api/v1/congestion/**").uri("http://localhost:8080"))
+               
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfigurationSource source = corsConfigurationSource();
+        return new CorsWebFilter( source);
     }
 }
